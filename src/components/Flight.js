e6e58506bd615f04b1f1e4a700bf5f68f5b942e2
Ricardo Guerra <RikkuG491@gmail.com>
2024-09-26 14:59:21 -0400

    Added images to db.json and rendered the images on the website

--- src/components/Flight.js
+++ src/components/Flight.js
@@ -1,11 +1,10 @@
 function Flight({flight}){
 
-    console.log(flight)
-
     return (
         <li>
             <h2>Flight # {flight.id}</h2>
             <h2>Airline: {flight.airline}</h2>
+            <img src={flight.image} alt={flight.airline}/>
         </li>
     );
 }
